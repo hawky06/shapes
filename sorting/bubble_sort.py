5edc1348b2ac2d09cab591ddb743e801db2bdd6e
Alex <athaug06@gmail.com>
2020-02-04 20:48:31 +0000

    optimise

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -2,13 +2,13 @@ def bubble_sort(unsorted):
     
     for i in range(len(unsorted) - 1):
         if (len(unsorted) > 1):
-            x = len(unsorted) - 1
-            while x > 0:
-                if (unsorted[x - 1] > unsorted[x]):
-                    temp = unsorted[x - 1]
-                    unsorted[x - 1] = unsorted[x]
+            x = 0
+            while x  < (len(unsorted) - 1 - i):
+                if (unsorted[x + 1] < unsorted[x]):
+                    temp = unsorted[x + 1]
+                    unsorted[x + 1] = unsorted[x]
                     unsorted[x] = temp 
-                x = x - 1
+                x = x + 1
         
     return unsorted
 
